d45b39fefd3d9a46e3ac967d00e69dae5e5ae072
jane muthoni <muchirijane@outlook.com>
2020-07-11 00:13:51 +0300

    finished animating user and chatbot

--- js/animation.js
+++ js/animation.js
@@ -1,4 +1,10 @@
 
 
-gsap.from("#chatbot-face", {duration: 7,  scale: 0.8, rotation: 360});
-gsap.to("#chatbot-face", {duration: 7, scale: 1.2,  ease: "sine.out", rotation: 360,  repeat: -1});
+gsap.from("#chatbot-face", {duration: 13, rotate: -15,  y: -10});
+gsap.to("#chatbot-face", {duration: 13,   ease: "sine.out", rotate: 15, y: 10, repeat: -1});
+
+gsap.from("#chatbot-body", {duration: 15,  scale: 0.8});
+gsap.to("#chatbot-body", {duration: 15, scale: 1,  ease: "bounce", x: 10, y: 20, repeat: -1});
+
+gsap.from("#female-avatar", {duration: 8,  scale: 0.4, rotation: 90,  y: 300, opacity: 0, repeat: -1});
+gsap.to("#female-avatar", {duration: 12, scale: 0.8,  ease: "bounce", repeat: -1});
